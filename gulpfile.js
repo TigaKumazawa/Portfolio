633c17660182855475833c60458471a1bf0d57c5
TigaKumazawa <68012429+TigaKumazawa@users.noreply.github.com>
2021-01-19 17:36:54 +0900

    Add files via upload

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+
+gulp.task("sass", function(){
+    gulp.src("sass/**/*.scss")
+    .pipe(sass({outputStyle:'expanded'}))
+    .pipe(gulp.dest("./css"))
+})
+
+gulp.task("default",function(){
+    gulp.watch("sass/**/*.scss",["sass"]);
+})
